267533c820c937868404eab87db164978e94f049
Lawrence Berry <lawrenceberry@outlook.com>
2021-04-25 15:13:16 +0100

    Source data from GitHub

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,7 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
 module.exports = {
   siteMetadata: {
     title: "personal-website",
@@ -9,6 +13,38 @@ module.exports = {
       resolve: "gatsby-plugin-typography",
       options: {
         pathToConfigModule: "src/utils/typography",
+      }
+    },
+    {
+      resolve: "gatsby-source-github-api",
+      options: {
+        token: process.env.GITHUB_PERSONAL_ACCESS_TOKEN,
+        graphQLQuery: `query {
+                        viewer {
+                          avatarUrl
+                          bio
+                          company
+                          email
+                          location
+                          login
+                          name
+                          twitterUsername
+                          updatedAt
+                          url
+                          websiteUrl
+                          repositories(privacy: PUBLIC, orderBy: {field: UPDATED_AT, direction: DESC}, first: 100, ownerAffiliations: OWNER) {
+                            nodes {
+                              description
+                              homepageUrl
+                              nameWithOwner
+                              pushedAt
+                              stargazerCount
+                              url
+                              updatedAt
+                            }
+                          }
+                        }
+                      }`,
       },
     },
   ],
