8abb8e92b965c82d1f17413c7cbd98cb7c5fb3aa
Lawrence Berry <lawrenceberry@outlook.com>
2021-05-08 15:10:12 +0100

    Add a blog page and links to both pages

---
+++ src/pages/blog.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+import { Layout } from "../components";
+
+const BlogPage = () => {
+  return <Layout>This is the blog page.</Layout>;
+};
+
+export default BlogPage;
