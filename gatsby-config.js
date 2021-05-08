aade76b8e0afc3d6cc039a01a6ab5ac1b844bb70
Lawrence Berry <lawrenceberry@outlook.com>
2021-05-08 17:30:50 +0100

    Liven up the 404 page

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,9 @@ module.exports = {
   plugins: [
     "gatsby-plugin-typescript",
     "gatsby-plugin-styled-components",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
     {
       resolve: "gatsby-plugin-typography",
       options: {
